e3e89578a13312e55da2a9fcb0f8415f5b8cd64c
Dwight Thorpe <dthorpe@day2dayja.com>
2024-10-02 23:31:10 -0500

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,14 +6,13 @@ declare global {
      * All the required environment variables for the server application
      */
     interface ProcessEnv {
+      // Admin API Key
       ADMIN_API_KEY: string;
-      //
+      // General
+      PORT: string;
       NODE_ENV: string;
       APP_NAME: string;
-      PORT: string;
-      LOCAL_URL: string;
       APP_CONTAINER_NAME: string;
-      PM2_CLUSTER_SIZE: string;
       MAX_UPLOAD_SIZE: string;
       // MySQL
       MYSQL_USER: string;
